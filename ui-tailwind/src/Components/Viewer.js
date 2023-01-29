35f2d3a32c3157dc55e1cb50963fa2872edd47b4
Imane Ajroudi <96076721+imane-aj@users.noreply.github.com>
2023-01-29 23:53:39 +0100

    thunksAPI REDUX

--- ui-tailwind/src/Components/Viewer.js
+++ ui-tailwind/src/Components/Viewer.js
@@ -3,7 +3,7 @@ import Modal from './Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import {animationStyles} from '../Utils/Const'
 import { getMeals } from './../Redux/mealsSlice';
-import { addFavorite, getFavorite } from './../Redux/favoriteSlice'; 
+import { addFavorite, getFavorite, deleteFavorite } from './../Redux/favoriteSlice'; 
 // import { handleModal } from '../Redux/modalSlice';
 import Pagination from './Pagination';
 
@@ -12,14 +12,11 @@ const Viewer = (show) => {
     const globalState = useSelector((state=>state))
     const [thisItem, setThisItem] = useState([])
     const [modal, setModal] = useState(false)
-    const [favIcon, setFavIcon] = useState(false)
+    const [favIcon, setFavIcon] = useState('+')
 
     //get meals data
-    useEffect(()=>{
-        dispatch(getMeals())
-        dispatch(getFavorite())
-    }, [dispatch])
-
+    useEffect(()=>{dispatch(getMeals()); dispatch(getFavorite())}, [dispatch])
+  
     //pagination
     const currentMeals = globalState.meals.meals?.slice(globalState.pagination.indexOfTheFirstMeal, globalState.pagination.indexOfLastMeal)
     const favorite = globalState.favorite.favorite?.slice(globalState.pagination.indexOfTheFirstMeal, globalState.pagination.indexOfLastMeal)
@@ -28,10 +25,9 @@ const Viewer = (show) => {
         const favorite = globalState.favorite.favorite
         if(favorite.length == 0 || !favorite.find((e) => e.strMeal == item.strMeal)){
             dispatch(addFavorite(item))
-
         }
     }
-    // const favorite = globalState.favorite.favorite
+    // const i = favorite.find((e) => e.strMeal == item.strMeal)
   return (
     <Fragment>
         {show.show ? (
@@ -39,7 +35,7 @@ const Viewer = (show) => {
             <div className='grid gap-8 grid-cols-2 lg:grid-cols-4'>
                 {favorite.length>0 ? (favorite?.map((item, idx)=>
                     <div className={` relative rounded px-3 py-2 border border-orange-300 ${animationStyles}`} key={idx}>
-                        <button className='absolute right-3 bottom-0 text-red-600'><i className="fa-solid fa-star text-lg"></i></button>
+                        <button onClick={()=>dispatch(deleteFavorite(item.id))} className='absolute right-3 bottom-0 text-red-600'><i className="fa-solid fa-star text-lg"></i></button>
                         <img src={item?.strMealThumb} alt='' className='h-44 w-full pb-3'/>
                         <div className='flex justify-between'>
                             <h6>{item?.strMeal}</h6>
@@ -58,7 +54,7 @@ const Viewer = (show) => {
                 <div className='grid gap-8 grid-cols-2 lg:grid-cols-4'>
                     {currentMeals.map((item, idx)=>
                         <div className={` relative rounded px-3 py-2 border border-orange-300 ${animationStyles}`} key={idx}>
-                            <button className='absolute right-3 bottom-0' onClick={()=>{selectData(item); setFavIcon(item)}}><i className={`${item.strMeal === favIcon.strMeal? 'fa-solid' : 'fa-regular'} fa-star text-lg`}></i></button>
+                            <button className='absolute right-3 bottom-0' onClick={()=>{selectData(item)}}><i className={`${favorite?.find((e) => e.strMeal == item.strMeal )? 'fa-solid' : 'fa-regular'} fa-star text-lg`}></i></button>
                             <img src={item.strMealThumb} alt='' className='h-44 w-full pb-3'/>
                             <div className='flex justify-between'>
                                 <h6>{item.strMeal}</h6>
--- ui-tailwind/src/Redux/favoriteSlice.js
+++ ui-tailwind/src/Redux/favoriteSlice.js
@@ -1,16 +1,22 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
 
 export const getFavorite = createAsyncThunk('favorite/getFavorite', async()=>{
     const res = await axios.get('http://localhost:8000/api/favorite')
     return res.data
 })
-export const addFavorite = createAsyncThunk('favorite/addFavorite', async(item)=>{
+export const addFavorite = createAsyncThunk('favorite/addFavorite', async(item, thunkAPI)=>{
     const res = await axios.post('http://localhost:8000/api/favorite',item)
+    thunkAPI.dispatch(getFavorite());
+})
+export const deleteFavorite = createAsyncThunk('favorite/addFavorite', async(id)=>{
+    const res = await axios.delete(`http://localhost:8000/api/favorite/${id}`)
+    return id 
 })
 const favoriteSlice = createSlice({
     name : "favorite",
-    initialState :{favorite : [], color: false, isLoading :false},
+    initialState :{favorite : [], isLoading :false},
     extraReducers: {
         [getFavorite.pending] : (state,action)=>{
             state.isLoading = true
@@ -20,8 +26,10 @@ const favoriteSlice = createSlice({
             state.favorite = action.payload
         },
         [addFavorite.fulfilled] : (state,action)=>{
-            state.color = true
             state.favorite.push(action.payload)
+        },
+        [deleteFavorite.fulfilled] : (state,action)=>{
+            state.favorite = state.favorite.filter((el) => el.id !== action.payload)
         }
     }
 })
